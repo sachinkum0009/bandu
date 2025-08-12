1fb03eae0e7e004303f3c96be6937b1cc25adbd1
Sachin Kumar <sachinkum123567@gmail.com>
2025-08-12 20:31:24 +0200

    starters added and minor changes

--- app.py
+++ app.py
@@ -8,7 +8,29 @@ Date: 2025-08-12
 import chainlit as cl
 from agents.basic_agent import create_agent
 
+history = []
+
 agent = create_agent()
+
+@cl.set_starters
+async def set_starters(user=None):
+    return [
+        cl.Starter(
+            label="Morning routine ideation",
+            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+            icon="/public/idea.png",
+        ),
+        cl.Starter(
+            label="What is Robot's Temperature?",
+            message="Can you help me understand the current temperature of the robot?",
+            icon="/public/thermometer.png",
+        ),
+        cl.Starter(
+            label="What does the robot see?",
+            message="Can you tell me what the robot is currently seeing?",
+            icon="/public/camera.png",
+        )
+    ]
 
 @cl.on_app_startup
 async def on_app_startup():
@@ -20,11 +42,12 @@ async def on_chat_start():
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    # Show spinner while agent is thinking
     msg = cl.Message(content="", author="Agent")
-    # Stream agent response
     full_content = ""
-    for chunk in agent.stream({"messages": [message.content]}):
+    history.append(message.content)
+    for chunk in agent.stream({"messages": history}):
+        if "thinker" not in chunk:
+            continue
         await msg.stream_token(chunk["thinker"]["messages"][-1].content)
         full_content += chunk["thinker"]["messages"][-1].content
     await msg.update()
